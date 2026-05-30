03e5f62d0a2f33ba7b7a58168fde21660287a6e2
sushil <sharmasushil265@gmail.com>
2026-05-30 14:00:59 +0530

    Update backend with unique storage account name

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "rg-terraform-state"
-    storage_account_name = "sttfstatedev001"
+    storage_account_name = "sttfstatedevsushil001"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
   }
