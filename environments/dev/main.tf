0be84c66ec0fbb98f2878843db62efd0e61b9218
sushil <sharmasushil265@gmail.com>
2026-05-30 13:46:48 +0530

    Remove remote backend configuration

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -5,13 +5,6 @@ terraform {
       version = "~> 3.0"
     }
   }
-
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-state"
-    storage_account_name = "sttfstatedev001" # This must be globally unique
-    container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
-  }
 }
 
 provider "azurerm" {
